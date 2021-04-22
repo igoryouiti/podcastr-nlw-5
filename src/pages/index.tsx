a5d00e2d81dd4a1b5293407f85e0f7970b40be34
Igor Youiti Sato <igoryouiti@hotmail.com>
2021-04-21 23:50:40 -0300

    últimos lançamentos in home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,10 +33,13 @@ export async function getServerSideProps() {
 // SSG -> static side generation só funciona no Next em produção
 
 import { GetStaticProps } from 'next';
+import Image from 'next/image';
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+
+import styles from './home.module.scss';
 
 type Episode = {
   id: string;
@@ -47,20 +50,52 @@ type Episode = {
   duration: number;
   durationAsString: string;
   url: string;
-  published_at: string;
-
+  publishedAt: string;
 }
 
 type HomeProps = {
-  episodes: Episode[];
+  latestEpisodes: Episode[];
+  allEpisodes: Episode[];
 }
 
-export default function Home(props: HomeProps) {
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 
   return (
-    <div>
-      <h1>Index</h1>
-      <p>{JSON.stringify(props.episodes)}</p>
+    <div className={styles.homepage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Últimos lançamentos</h2>
+
+        <ul>
+          {latestEpisodes.map(episode => {
+            return (
+              <li key={episode.id}>
+                <Image
+                  width={192}
+                  height={192}
+                  src={episode.thumbnail}
+                  alt={episode.title}
+                  objectFit="cover"
+                />
+
+                <div className={styles.episodeDetails}>
+                  <a href="">{episode.title}</a>
+                  <p>{episode.members}</p>
+                  <span>{episode.publishedAt}</span>
+                  <span>{episode.durationAsString}</span>
+                </div>
+
+                <button type="button">
+                  <img src="/play-green.svg" alt="Tocar episódio" />
+                </button>
+              </li>
+
+            )
+          })}
+        </ul>
+      </section>
+      <section className={styles.allEpisodes}>
+
+      </section>
     </div>
   )
 }
@@ -80,17 +115,21 @@ export const getStaticProps: GetStaticProps = async () => {
       title: episode.title,
       thumbnail: episode.thumbnail,
       members: episode.members,
-      published_at: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
       duration: Number(episode.file.duration),
       durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
       description: episode.description,
       url: episode.file.url,
-    }
+    };
   })
+
+  const latestEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length)
 
   return {
     props: {
-      episodes
+      latestEpisodes,
+      allEpisodes
     },
     revalidate: 60 * 60 * 8 // 60 sec * 60 min * 8h = 28.800 sec (8h)
   }
