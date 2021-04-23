a540c7d5e8664f3cee8ea57f5e7c899ad9ad3fe0
Igor Youiti Sato <igoryouiti@hotmail.com>
2021-04-23 18:01:06 -0300

    add favicon and title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,6 +34,7 @@ export async function getServerSideProps() {
 
 import { GetStaticProps } from 'next';
 import Image from 'next/image';
+import Head from 'next/head';
 import Link from 'next/link';
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
@@ -67,6 +68,9 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 
   return (
     <div className={styles.homepage}>
+      <Head>
+        <title>Home | Podcastr</title>
+      </Head>
       <section className={styles.latestEpisodes}>
         <h2>Últimos lançamentos</h2>
 
